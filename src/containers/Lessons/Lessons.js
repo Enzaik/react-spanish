fd2dec873516234d63bb5f48590393d896420e9d
enzaik <imernestodiaz@gmail.com>
2019-08-14 23:16:18 +0200

    Add students to firebase and fetch them

--- src/containers/Lessons/Lessons.js
+++ src/containers/Lessons/Lessons.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
-import MoodIcon from '@material-ui/icons/Mood';
-import DraftsIcon from '@material-ui/icons/Drafts';
+import SchoolIcon from '@material-ui/icons/School';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import axios from '../../axios-instance';
+
 import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles(theme => ({
@@ -21,38 +23,61 @@ function ListItemLink(props) {
   return <ListItem button component="a" {...props} />;
 }
 
-export default function SimpleList() {
- return (
-    <Container >
-      <List component="nav" aria-label="main mailbox folders">
-        <ListItem button>
-          <ListItemIcon>
-            <MoodIcon />
-          </ListItemIcon>
-          <ListItemText primary="Inbox" />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <MoodIcon />
-          </ListItemIcon>
-          <ListItemText primary="Drafts" />
-        </ListItem>
-      </List>
-      <Divider />
-      <List component="nav" aria-label="secondary mailbox folders">
-      <ListItem button>
-          <ListItemIcon>
-            <MoodIcon />
-          </ListItemIcon>
-          <ListItemText primary="Inbox" />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <MoodIcon />
-          </ListItemIcon>
-          <ListItemText primary="Drafts" />
-        </ListItem>
-      </List>
-    </Container>
-  );
+export default class Lessons extends Component {
+
+  state= {messages: null}
+
+componentDidMount(){
+  axios.get('students.json')
+  .then(res=>{
+    
+    this.setState({...this.state, messages: res.data})
+    console.log(this.state);
+  })
+}
+
+
+  render(){
+
+let states = <LinearProgress />;
+
+if(this.state.messages !== null){
+  states = (
+    this.state.messages.map(state =>{
+      return (<ListItem button>
+        <ListItemIcon>
+          <SchoolIcon />
+        </ListItemIcon>
+        <ListItemText primary={state.name} />
+      </ListItem>)
+    })
+  )
+}
+
+
+
+    return (
+      <Container >
+        <List component="nav" aria-label="main mailbox folders">
+          {states}
+        </List>
+        <Divider />
+        <List component="nav" aria-label="secondary mailbox folders">
+        {/* <ListItem button>
+            <ListItemIcon>
+              <SchoolIcon />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <SchoolIcon />
+            </ListItemIcon>
+            <ListItemText primary="Drafts" />
+          </ListItem> */}
+        </List>
+      </Container>
+    );
+  }
+ 
 }
